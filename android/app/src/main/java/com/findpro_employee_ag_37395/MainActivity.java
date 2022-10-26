859841a756265e87e9c2bf9ade3dea881418241a
Crowdbotics <team@crowdbotics.com>
2022-10-26 17:46:37 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/findpro_employee_ag_37395/MainActivity.java
@@ -0,0 +1,15 @@
+package com.findpro_employee_ag_37395;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "findpro_employee_ag_37395";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "findpro_employee_ag_37395",
+  url: "https://findpro_employee_ag_37395.botics.co",
+  api: "https://findpro_employee_ag_37395.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
